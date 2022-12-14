7e30017d472dfc4f8369ac115994f6373ce11cbc
alhad-tm <alhadchikkutm@gmail.com>
2022-12-15 01:23:20 +0530

    added more genre row

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React from 'react';
-import {originals,action} from "./urls"
+import {originals,action,HorrorMovies,ComedyMovies,RomanceMovies,Documentaries,Trending} from "./urls"
 import './App.css';
 import Banner from './components/banner/Banner';
 import Navbar from './components/Navbar/Navbar';
 import Rowpost from './components/rowpost/Rowpost';
+import Details from './components/Details/Details';
 
 
 function App() {
   return (
     <div className="App">
+    
+     
     <Navbar/>
     <Banner/>
-    <Rowpost  url ={originals} title="Netflix Originals"/>
+    <Rowpost url ={originals} title="Netflix Originals"/>
+    <Rowpost url={Trending} title="Trending" isSmall />
     <Rowpost url={action} title="Action" isSmall />
-  
-    </div>
+    <Rowpost url={HorrorMovies} title="Horror Movies" isSmall />
+    <Rowpost url={ComedyMovies} title="Comedy Movies" isSmall />
+    <Rowpost url={RomanceMovies} title="Romance Movies" isSmall />
+    <Rowpost url={Documentaries} title="Documentaries" isSmall />
+     {/* <Details/> */}
+      </div>
   );
 }
 
--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -8,21 +8,21 @@ function Banner() {
   const [movie, setMovie] = useState()
   useEffect(()=>{
      axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
-      console.log(response.data.results[2]);
+      console.log(response.data.results);
       setMovie(response.data.results[1])
      })
     }, [])
   return (
     <div  style={{backgroundImage:`url(${movie? imageUrl + movie.backdrop_path: "" })`}} className='banner'>
         <div className='content'>
-            <h1 className='title'>{movie ? movie.title : ""}</h1>
+            <h1 className='title'>{movie ? movie.title: ""}</h1>
             <div className='banner_buttons'>
                 <button className='button'>Play</button>
                 <button className='button'>My List</button>
             </div>
             <h2 className='description'>{movie? movie.overview : ""}.</h2>
         </div>
-     <div className="fade_bottom"></div>   
+     {/* <div className="fade_bottom"></div>    */}
      
 
     </div>
